ddb1bf8738b6285c2f3f23477c771859345a301f
Nata Kononets <Kononets.n.g@gmail.com>
2026-01-14 12:47:04 +0100

    14.01

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD6nPhKPd6OkwepzSlnHPO41YNXYiVlipM",
-  authDomain: "tup-tup-shop.firebaseapp.com",
-  projectId: "tup-tup-shop",
-  storageBucket: "tup-tup-shop.firebasestorage.app",
-  messagingSenderId: "89601565536",
-  appId: "1:89601565536:web:2106fa359493e26b8a7405",
-  measurementId: "G-WBD257MSSY"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
